a787697edf78569c98b60592cce89a1117111775
finalcutgit411 <finalcutgit411@gmail.com>
2016-06-30 01:11:19 +0200

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -71,19 +71,19 @@ contacter votre service technique et demander l'activation du module TUN/TAP"
 					OPTIONS="0"
                         		while [[ -z "$OS" ]]; do
 					clear
-                        		read -p "Je n'ai pas reussi à récuperer la version de votre distibution.
-Est ce bien un des systèmes d'exploitation ci-dessous ?
+                        		read -p " Je n'ai pas reussi à récuperer la version de votre distibution Debian
+
 1 ) Debian 8  Jessie
 2 ) Debian 7  Wheezy
 
 Q ) Taper Q pour quitter
 
-Si oui merci de me l'indiquer [1-2]: " -r OPTIONS
+Merci de me l'indiquer [1-2]: " -r OPTIONS
                                 		case "$OPTIONS" in
                                         		1) OS="jessie" ;;
                                         		2) OS="wheezy" ;;
-                                        		Q) MESSAGE="Si votre systeme d'exploitation n'est pas référencé, si vous etes bien 
-sur un serveur basé sur Debian vous pouvez forcer l'installation à vos risques et
+                                        		Q) MESSAGE="Si votre systeme d'exploitation n'est pas référencé, si vous etes
+sur un Debian like vous pouvez forcer l'installation à vos risques et
 périls en choisissant l'option Jessie (systemd) ou Wheezy (init)" && quitter
                                 		esac
                         		done
